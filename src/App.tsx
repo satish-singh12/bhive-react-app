e4bc3c377ed8e32ad99eb7588615e501b26eb90d
Satish Singh <satishsingh12@gmail.com>
2025-06-06 08:15:32 +0530

    Initial Commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,10 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
+  const apiUrl = process.env.REACT_APP_API_URL;
+console.log("API URL:", apiUrl);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App bg-gray-300">
+    <h1 className="text-3xl font-bold text-center">BHIVE FRONTENT APP</h1>
     </div>
   );
 }
